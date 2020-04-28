0e91ce3d29b2aea2911d11161c7bda783e428092
weitong <weitong@ebupt.com>
2020-04-28 20:43:43 +0800

    Initial commit 1.0

---
+++ spring-cloud-netflix/eureka-server/src/main/java/com/kyara/springcloud/netflix/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.kyara.springcloud.netflix.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @desc 启动一个注册中心 @EnableEurekaServer
+ * @author watt
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
